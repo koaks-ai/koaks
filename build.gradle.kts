0db697a606153352a2817bb55e97b009894c67e4
gmjneko <gemingjia0201@163.com>
2025-07-19 14:59:41 +0800

    chore: add jsoup

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,6 @@ plugins {
 group = "org.endow.framework"
 version = "0.0.1-SNAPSHOT"
 
-val ktor = "3.2.1"
-val logback = "1.5.13"
 
 repositories {
     mavenCentral()
@@ -15,13 +13,12 @@ repositories {
 
 
 dependencies {
-//    implementation("io.ktor:ktor-client-core:${ktor}")
-//    implementation("io.ktor:ktor-client-cio:${ktor}")
-    implementation("ch.qos.logback:logback-classic:${logback}")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("org.reflections:reflections:0.10.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework:spring-webflux:6.2.3")
+    implementation("org.jsoup:jsoup:1.21.1")
 //    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("com.google.code.gson:gson:2.13.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
