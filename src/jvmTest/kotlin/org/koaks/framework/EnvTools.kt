769ba0e7bbcc81bea1a650ddb82e1e77acd1eca2
mynna404 <gemingjia0201@163.com>
2025-08-21 14:43:46 +0800

    refactor(test): update EnvTools for .env file

--- src/jvmTest/kotlin/org/koaks/framework/EnvTools.kt
+++ src/jvmTest/kotlin/org/koaks/framework/EnvTools.kt
@@ -5,10 +5,10 @@ import io.github.cdimascio.dotenv.dotenv
 object EnvTools {
 
     val dotenv = dotenv {
-        directory = "src/jvmTest/kotlin/org/koaks/framework"
         ignoreIfMalformed = true
         ignoreIfMissing = true
     }
 
     fun loadValue(key: String): String = dotenv[key]
+
 }
