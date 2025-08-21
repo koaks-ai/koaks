fd9aede80775d8814b7266ada4621fa8d21c03a0
mynna404 <gemingjia0201@163.com>
2025-08-21 21:04:46 +0800

    build: migrate to libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "2.1.21" apply false
-    kotlin("plugin.serialization") version "2.1.21" apply false
-    id("com.vanniktech.maven.publish") version "0.34.0" apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.maven.publish) apply false
 }
 
 subprojects {
