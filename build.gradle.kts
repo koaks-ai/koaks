6670d921e42e086e4e07153398e53a9632fd0037
mynna404 <gemingjia0201@163.com>
2025-08-21 14:43:07 +0800

    feat(common,jvmMain/net): replace okhttp3 with ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 // only test publish
 group = "io.github.mynna404"
-version = "0.0.1-beta6"
+version = "0.0.1-kmp-preview1"
 
 
 repositories {
@@ -16,42 +16,43 @@ repositories {
 kotlin {
     jvmToolchain(21)
 
-    jvm {
-//        testRuns["test"].executionTask.configure {
-//            useJUnitPlatform()
-//        }
+    jvm {}
+
+    js(IR) {
+        nodejs()
     }
 
-//    js(IR) {
-//        browser()
-//        nodejs()
-//    }
-
     sourceSets {
+
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
                 implementation("io.github.oshai:kotlin-logging:7.0.3")
+                implementation("io.ktor:ktor-client-core:3.2.3")
+
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
             }
         }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
+
         val jvmMain by getting {
             dependencies {
+                implementation("io.ktor:ktor-client-cio:3.2.3")
                 implementation("ch.qos.logback:logback-classic:1.5.13")
                 implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
                 implementation("org.reflections:reflections:0.10.2")
                 implementation("org.jetbrains.kotlin:kotlin-reflect")
-                implementation("com.squareup.okhttp3:okhttp:5.1.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
             }
         }
+
         val jvmTest by getting {
             dependencies {
                 implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
@@ -59,17 +60,20 @@ kotlin {
                 implementation("org.jetbrains.kotlin:kotlin-test-junit5")
             }
         }
-//        val jsMain by getting {
-//            dependencies {
-//                implementation("io.github.oshai:kotlin-logging-js:7.0.3")
-//                implementation("io.ktor:ktor-client-js:2.3.11")
-//            }
-//        }
-//        val jsTest by getting {
-//            dependencies {
-//                implementation("org.jetbrains.kotlin:kotlin-test-js")
-//            }
-//        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-js:3.2.3")
+                implementation("io.github.oshai:kotlin-logging-js:7.0.3")
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-test-js")
+            }
+        }
+
     }
 
 }
