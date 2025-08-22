83be9f11fcaab7cd85a4c9a2e67a0c2ec1e89d98
gmjneko <gemingjia0201@163.com>
2025-08-22 18:13:58 +0800

    build(build-logic): migrate submodule build logic into build-logic convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,8 @@
-plugins {
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.maven.publish) apply false
-}
-
-subprojects {
-
-    apply {
-        plugin("kotlin-multiplatform")
-        plugin("kotlinx-serialization")
-        plugin("com.vanniktech.maven.publish")
-    }
-
-    // only test publish
+allprojects {
     group = "io.github.mynna404"
     version = "0.0.1-preview1"
 
     repositories {
         mavenCentral()
     }
-
 }
