f41d0f8ebd6e3abf5f4ad8e567cd51dfca250c4d
gmjneko <gemingjia0201@163.com>
2025-08-22 19:24:59 +0800

    build: bump foojay-resolver-convention to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,14 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-    includeBuild("build-logic")
+
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "koaks"
 
+includeBuild("build-logic")
 include("core")
