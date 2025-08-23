974bb141b8d72755d68552154eeb3dc284025a48
gmjneko <gemingjia0201@163.com>
2025-08-23 22:59:56 +0800

    refactor(core/commonMain,jvmMain): split handle both annotation and interface tools

---
+++ core/src/commonMain/kotlin/org/koaks/framework/toolcall/caller/AnnoTypeCaller.kt
@@ -0,0 +1,10 @@
+package org.koaks.framework.toolcall.caller
+
+import org.koaks.framework.toolcall.ToolDefinition
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect object AnnoTypeCaller {
+
+    suspend fun call(tool: ToolDefinition, args: Array<Any>?): String
+
+}
