5367c40f7bcfde5bead272126ccc6f9e69a37500
mynna404 <gemingjia0201@163.com>
2025-08-23 14:53:28 +0800

    feat(core/commonMain): ToolCreater add group params

--- core/src/commonMain/kotlin/org/koaks/framework/toolcall/ToolCreater.kt
+++ core/src/commonMain/kotlin/org/koaks/framework/toolcall/ToolCreater.kt
@@ -4,11 +4,13 @@ package org.koaks.framework.toolcall
 class ToolBuilder<T> {
     var name: String = ""
     var description: String = ""
+    var group: String = "default"
     lateinit var execute: suspend (T) -> String
 
     fun build(): Tool<T> = object : Tool<T> {
         override val name = this@ToolBuilder.name
         override val description = this@ToolBuilder.description
+        override val group = this@ToolBuilder.group
         override suspend fun execute(input: T): String = this@ToolBuilder.execute(input)
     }
 }
@@ -20,11 +22,13 @@ inline fun <reified T> createTool(block: ToolBuilder<T>.() -> Unit): Tool<T> {
 fun <T> createTool(
     name: String,
     description: String,
+    group: String = "default",
     block: suspend (T) -> String
 ): Tool<T> {
     return object : Tool<T> {
         override val name = name
         override val description = description
+        override val group = group
         override suspend fun execute(input: T): String = block(input)
     }
 }
