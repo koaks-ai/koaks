dc0ee7b5e072463ba4615226488b89633390021c
mynna <gemingjia0201@163.com>
2025-08-27 16:32:14 +0800

    feat(core): add platform type check

--- core/src/commonMain/kotlin/org/koaks/framework/Koaks.kt
+++ core/src/commonMain/kotlin/org/koaks/framework/Koaks.kt
@@ -2,6 +2,8 @@ package org.koaks.framework
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.koaks.framework.context.KoaksContext
+import org.koaks.framework.platform.PlatformType
+import org.koaks.framework.platform.PlatformUtils
 import org.koaks.framework.toolcall.ToolInitializer
 
 object Koaks {
@@ -9,10 +11,12 @@ object Koaks {
     private val logger = KotlinLogging.logger {}
 
     fun init(vararg packageName: String) {
-        packageName.forEach {
-            KoaksContext.addPackageName(it)
+        if (PlatformUtils.platformType() == PlatformType.JVM) {
+            packageName.forEach {
+                KoaksContext.addPackageName(it)
+            }
+            ToolInitializer.init()
         }
-        ToolInitializer.init()
         logger.info { "Koaks initialized" }
     }
 
