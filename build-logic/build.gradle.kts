86a4cac7c865d0b5e4b91b1064465b6cc677cdee
mynna <gemingjia0201@163.com>
2025-08-29 21:19:19 +0800

    build: migrate shared build configuration to build-logic

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.8.1")
+    implementation("com.vanniktech:gradle-maven-publish-plugin:0.34.0")
+}
+
+gradlePlugin {
+    plugins {
+        register("kmpLibrary") {
+            id = "koaks.kmp.library"
+            implementationClass = "KmpLibraryConventionPlugin"
+        }
+        register("kmpPublishing") {
+            id = "koaks.kmp.publishing"
+            implementationClass = "KmpPublishConventionPlugin"
+        }
+        register("rootConvention") {
+            id = "koaks.root.convention"
+            implementationClass = "RootConventionPlugin"
+        }
+    }
+}
--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -1,45 +1,9 @@
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
-
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.maven.publish)
+    id("koaks.kmp.library")
 }
 
 kotlin {
     sourceSets {
-        jvmToolchain(21)
-
-        jvm()
-
-        js(IR) {
-            browser {
-                testTask {
-                    enabled = false
-                    useKarma {
-                        useChromeHeadless()
-                        useConfigDirectory(project.rootProject.file("karma"))
-                    }
-                }
-            }
-            nodejs {
-                testTask {
-                    useMocha {
-                        timeout = "0"
-                    }
-                    environment("PROJECT_ROOT", rootDir.absolutePath)
-                }
-            }
-            binaries.executable()
-        }
-
-        macosArm64("macosArm") {
-            binaries {
-                executable()
-            }
-        }
-
         val commonTest by getting {
             dependencies {
                 implementation(project(":core"))
@@ -48,7 +12,6 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-
         val jvmTest by getting {
             dependencies {
                 implementation(libs.dotenv)
@@ -56,31 +19,18 @@ kotlin {
                 implementation(libs.kotlin.test.junit5)
             }
         }
-
         val jsTest by getting {
             dependencies {
                 implementation(npm("dotenv", "17.2.1"))
             }
         }
-
     }
-
 }
 
-// for kotlin/js nodejs
-//project.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin> {
-//    // use local nodejs
-//    project.extensions.getByType<NodeJsEnvSpec>().download.set(false)
-//}
-
-//rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-//    // this `download` will be removed in version 2.2, need migrate
-//    // use local yarn
-//    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().download =  false
-//}
-
-tasks.withType<KotlinJsCompile>().configureEach {
-    compilerOptions {
-        target.set("es2015")
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+        showExceptions = true
     }
 }
