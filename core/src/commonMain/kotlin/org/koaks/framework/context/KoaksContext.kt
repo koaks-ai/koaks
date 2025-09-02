79c5e7e3df11b4f0609ccb38d4201ac76f12a7eb
TruE <gemingjia0201@163.com>
2025-09-02 13:45:57 +0800

    feat(core/common/context): add tool registration to context

--- core/src/commonMain/kotlin/org/koaks/framework/context/KoaksContext.kt
+++ core/src/commonMain/kotlin/org/koaks/framework/context/KoaksContext.kt
@@ -4,6 +4,8 @@ object KoaksContext {
 
     private var scanPackageName: MutableList<String> = mutableListOf()
 
+    private val clientToolContainer: MutableMap<String, MutableList<String>> = mutableMapOf()
+
     init {
         this.scanPackageName.add("org.koaks.framework")
     }
@@ -14,6 +16,12 @@ object KoaksContext {
 
     fun addPackageName(packageName: String) {
         this.scanPackageName.add(packageName)
+    }
+
+    fun registerTool(clientId: String, toolName: String) {
+        clientToolContainer[clientId]?.add(toolName) ?: run {
+            clientToolContainer[clientId] = mutableListOf(toolName)
+        }
     }
 
 }
