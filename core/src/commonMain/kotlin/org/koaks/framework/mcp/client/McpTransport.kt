2fd1b436f85f55b53ca17ca4ea24e8cd68c98002
gmjneko <gemingjia0201@163.com>
2025-09-22 22:11:02 +0800

    feat(mcp/common): update McpTransport interface

--- core/src/commonMain/kotlin/org/koaks/framework/mcp/client/McpTransport.kt
+++ core/src/commonMain/kotlin/org/koaks/framework/mcp/client/McpTransport.kt
@@ -1,10 +1,22 @@
 package org.koaks.framework.mcp.client
 
-import kotlinx.serialization.json.JsonElement
+import kotlinx.coroutines.flow.Flow
+import kotlinx.serialization.KSerializer
 import org.koaks.framework.mcp.entity.McpMessage
 
 interface McpTransport {
-    suspend fun sendRequest(method: String, params: Any? = null): JsonElement
-    suspend fun sendNotification(method: String, params: Any? = null)
-    suspend fun listenForMessages(onMessage: (McpMessage) -> Unit)
+    suspend fun <Req, Resp> request(
+        method: String,
+        params: Req? = null,
+        requestSerializer: KSerializer<Req>? = null,
+        responseSerializer: KSerializer<Resp>
+    ): Resp
+
+    suspend fun <Req> notify(
+        method: String,
+        params: Req? = null,
+        serializer: KSerializer<Req>? = null
+    )
+
+    fun messages(): Flow<McpMessage>
 }
